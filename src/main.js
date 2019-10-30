5cf008a57b8d47fe5b977427f60f690d64861b3f
another20190216 <1608152475@qq.com>
2019-10-30 13:48:57 +0800

    "配置路由、按需加载element组件"

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import { upload, dialog } from 'element-ui'
 import App from './App.vue'
 import router from './router'
 
+Vue.use(upload).use(dialog)
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'home',
-    component: Home
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '*',
+    component: () => import(/* webpackChunkName: "404" */ '../views/404')
   }
 ]
 
