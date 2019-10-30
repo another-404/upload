27f3e25db424a8990b4f122b79a4cd7463f70d94
another20190216 <1608152475@qq.com>
2019-10-30 15:10:43 +0800

    "使用element官方upload组件demo"

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="home">
-    
+    <upLoad></upLoad>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
-
+import upLoad from "@/components/upload";
 export default {
-  name: 'home',
-  components: {
-    
-  }
-}
+  name: "home",
+  components: { upLoad }
+};
 </script>
